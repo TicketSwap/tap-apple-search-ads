167dc9dbcf77a32186e0bb59637903d0bbb0f0b9
Tobias Cadee <tobias.cadee@ticketswap.com>
2024-10-10 11:31:31 +0200

    add `GranularReportsStream` class
    adding a class to support granular report streams and `CampaignGranularReportsStream` to implement this for campaign reports.

--- tap_apple_search_ads/streams.py
+++ tap_apple_search_ads/streams.py
@@ -3,13 +3,16 @@
 from __future__ import annotations
 
 import typing as t
-from datetime import datetime, timezone
+from datetime import datetime, timedelta, timezone
+
+from singer_sdk.helpers.jsonpath import extract_jsonpath
 
 from tap_apple_search_ads.client import AppleSearchAdsStream
 
 from .schemas import campaigns_schema, reports_schema
 
 if t.TYPE_CHECKING:
+    import requests
     from singer_sdk.helpers.types import Context, Record
 
 _TToken = t.TypeVar("_TToken")
@@ -44,7 +47,7 @@ class ReportStream(AppleSearchAdsStream):
 
     def prepare_request_payload(
         self,
-        context: Context | None,  # noqa: ARG002
+        context: Context | None,
         next_page_token: _TToken | None,  # noqa: ARG002
     ) -> dict | None:
         """Prepare the data payload for the REST API request.
@@ -56,10 +59,7 @@ class ReportStream(AppleSearchAdsStream):
         """
         return {
             "startTime": self.config.get("start_date", "2016-01-01"),
-            "endTime": self.config.get(
-                "start_date",
-                datetime.now(tz=timezone.utc).strftime("%Y-%m-%d"),
-            ),
+            "endTime": self.config.get("end_date"),
             "selector": {
                 "orderBy": [{"field": self.primary_keys[0], "sortOrder": "ASCENDING"}],
                 "pagination": {"offset": 0, "limit": 1000},
@@ -69,6 +69,19 @@ class ReportStream(AppleSearchAdsStream):
             "returnRowTotals": True,
             "returnGrandTotals": True,
         }
+
+    def parse_response(self, response: requests.Response) -> t.Iterable[dict]:
+        """Parse the response and return an iterator of result records.
+
+        Args:
+            response: A raw :class:`requests.Response`
+
+        Yields:
+            One item for every item found in the response.
+        """
+        for record in extract_jsonpath(self.records_jsonpath, input=response.json()):
+            record["total"]["metadata"] = record["metadata"]
+            yield record["total"]
 
     def post_process(
         self,
@@ -84,9 +97,102 @@ class ReportStream(AppleSearchAdsStream):
         Returns:
             The resulting record dict, or `None` if the record should be excluded.
         """
-        new_row = dict(row["total"].items())
-        new_row[self.primary_keys[0]] = row["metadata"][self.primary_keys[0]]
-        return new_row
+        row[self.primary_keys[0]] = row["metadata"][self.primary_keys[0]]
+        return row
+
+
+class GranularReportsStream(ReportStream):
+    """Base class for report streams.
+
+    This stream returns granular results for reports set by `report_granularity`
+    """
+
+    replication_key = "date"
+
+    def prepare_request_payload(
+        self,
+        context: Context | None,
+        next_page_token: _TToken | None,
+    ) -> dict | None:
+        """Prepare the data payload for the REST API request.
+
+        Args:
+            context: Stream partition or context dictionary.
+            next_page_token: Token, page number or any request argument to request the
+                next page of data.
+        """
+        payload = super().prepare_request_payload(context, next_page_token)
+        granularity = self.config["report_granularity"]
+        payload["granularity"] = granularity
+        match granularity:
+            case "HOURLY":
+                days, min_interval, max_interval = 30, 0, 7
+            case "DAILY":
+                days, min_interval, max_interval = 90, 0, 90
+            case "WEEKLY":
+                days, min_interval, max_interval = 365 * 2, 14, 365
+            case "MONTHLY":
+                days, min_interval, max_interval = 365 * 2, 31 * 3, 365 * 2
+        now = datetime.now(tz=timezone.utc)
+        min_start_date = now - timedelta(days=days)
+        start_date = datetime.fromisoformat(
+            self.get_starting_replication_key_value(
+                context,
+            )
+            or self.config.get("start_date")
+            or "1900-01-01",
+        )
+        start_date = start_date.replace(tzinfo=timezone.utc)
+        if start_date < min_start_date:
+            start_date = min_start_date
+            self.logger.info(
+                (
+                    "Start date is before minimum start date for this "
+                    "granularity, setting start date to %s"
+                ),
+                start_date.strftime("%Y-%m-%d"),
+            )
+        start_date = start_date.replace(tzinfo=timezone.utc)
+        min_interval_start_date = now - timedelta(days=min_interval)
+        if start_date > min_interval_start_date:
+            start_date = min_interval_start_date
+            self.logger.info(
+                (
+                    "Start date is after minimum interval date for this "
+                    "granularity, setting start date to %s"
+                ),
+                start_date.strftime("%Y-%m-%d"),
+            )
+        end_date = now
+        max_end_date = start_date + timedelta(max_interval)
+        if max_end_date < end_date:
+            end_date = max_end_date
+            self.logger.info(
+                (
+                    "End date is after maximum end date for this "
+                    "granularity, setting end date to %s"
+                ),
+                end_date.strftime("%Y-%m-%d"),
+            )
+        payload["endTime"] = end_date.strftime("%Y-%m-%d")
+        payload["startTime"] = start_date.strftime("%Y-%m-%d")
+        payload["returnRowTotals"] = False
+        payload["returnGrandTotals"] = False
+        return payload
+
+    def parse_response(self, response: requests.Response) -> t.Iterable[dict]:
+        """Parse the response and return an iterator of result records.
+
+        Args:
+            response: A raw :class:`requests.Response`
+
+        Yields:
+            One item for every item found in the response.
+        """
+        for record in extract_jsonpath(self.records_jsonpath, input=response.json()):
+            for granular_record in record["granularity"]:
+                granular_record["metadata"] = record["metadata"]
+                yield granular_record
 
 
 class CampaignReportsStream(ReportStream):
@@ -97,3 +203,13 @@ class CampaignReportsStream(ReportStream):
         "campaignId",
     ]  # make sure this is just one key for report streams.
     name = "campaign_reports"
+
+
+class CampaignGranularReportsStream(GranularReportsStream):
+    """Campaign granular reports stream."""
+
+    path = "/reports/campaigns"
+    primary_keys: t.ClassVar[list[str]] = [
+        "campaignId",
+    ]  # make sure this is just one key for report streams.
+    name = "campaign_granular_reports"
